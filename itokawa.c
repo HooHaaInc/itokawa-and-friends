23f96f6d7b34ff1c171141c0ad0df6c48cfa7f6a
JorgeACS <jorge_nintendo@hotmail.com>
2016-05-18 20:53:56 -0700

    No puedo compilar xD

--- itokawa.c
+++ itokawa.c
@@ -1,3 +1,10 @@
+/*
+    Titulo: Proyecto Final Graficacion
+    Descripcion: Proyecciones de objetos 3D a 2D, obtenidos de archivos
+    Autores: Nan y Jorge
+    Fecha: 5/18/2016
+*/
+
 // -lGLEW
 #include <GL/glew.h>
 // -lglut
@@ -7,12 +14,12 @@
 #include <math.h>
 // -lgl
 
-
+//Estructura triangulo para manejar los puntos de los archivos
 struct Trianglew
 {
 	float p1[4], p2[4], p3[4];
 };
-
+//Prototipos de funciones
 void mat_mul(float[16], float[], int, float[]);
 void mat_id(float[16]);
 void mat_trans(float[16], float, float, float);
@@ -34,16 +41,19 @@ void abrir_archivo_y_procesa(struct Trianglew[]);
 void setup();
 void display();
 void draw_pixel(float*, int, int, float, float, float);
+//Ancho y alto de la pantalla
+int width = 2, height = 2;
+//Numero de triangulos en el archivo itokawa
+int N=49152;
 
-int width = 480, height = 480, N=49152;
 struct Trianglew triangles[49152];
-
+//Main
 int main(int argc, char *argv[])
 {
 	abrir_archivo_y_procesa(triangles);
 	glutInit(&argc, argv);
 	glutInitDisplayMode(GLUT_RGB | GLUT_DEPTH | GLUT_DOUBLE);
-	glutInitWindowSize(width,height);
+	glutInitWindowSize(600,600);
 	glutCreateWindow("Itokawa");
 
 	setup();
@@ -51,23 +61,34 @@ int main(int argc, char *argv[])
 	glutMainLoop();
 	return 0;
 }
-
+//Funcion de dibujado de OpenGL
 void display(){
+    //Limpiamos el display
 	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-	//glColor3f(0.0f, 0.0f, 0.0f);
-	//glRectf(-0.75f,0.75f, 0.75f, -0.75f);
-	
-	float *a = (float*)malloc(sizeof(float)*height*width*3);
 
+    //Pedimos memoria para la matriz tridimensional de dibujo
+	float *a = (float*)malloc(sizeof(float)*height*width*3);
+    //Plano de proyeccion (0 = xy, 1 = xz, 2 = yz)
+    int plano = 0;
+    //Declaracion de la matriz compuesta
 	float m[16];
+	//Calculamos la matriz compuesta
 	calc_mat(m);
+	//Empezamos la interfaz de openGL para dibujar lineas
+	glBegin(GL_LINES);
+	//Definimos color de linea blanco
+	glColor3f(1.0f, 1.0f, 1.0f);
+	//Proyectamos y dibujamos todos los puntos
 	for(int i=0; i<N; ++i){
-		proyectar(m, a, triangles[i]);
+		proyectar(m, a, triangles[i],plano);
 	}
-	glDrawPixels(width, height, GL_RGB, GL_FLOAT, a);
+	//Cerramos la interfaz de openGL
+	glEnd();
+	//No se, lolz
 	glutSwapBuffers();
 }
-
+//Multiplicacion de matrices
+//m2 no es de longitud fija ya que a esta funcion se le puede enviar un vector tambien
 void mat_mul(float m1[16], float m2[], int n, float mat[]){
 	float column[4];
     for (int j=0; j<n; ++j){
@@ -79,35 +100,35 @@ void mat_mul(float m1[16], float m2[], int n, float mat[]){
         for (int i=0; i<4; ++i)
             mat[i*n+j] = column[i];
     }
-    
-}
 
+}
+//Matriz identidad
 void mat_id(float mat[16]){
-    
-              mat[1]  = mat[2] = mat[3]  = 
-    mat[4]  =           mat[6] = mat[7]  = 
-    mat[8]  = mat[9]  =          mat[11] = 
+
+              mat[1]  = mat[2] = mat[3]  =
+    mat[4]  =           mat[6] = mat[7]  =
+    mat[8]  = mat[9]  =          mat[11] =
     mat[12] = mat[13] = mat[14]          = 0;
-    
+
     mat[0]  = mat[5]  = mat[10]= mat[15] = 1;
 
 }
-
+//Matriz de translacion
 void mat_trans(float mat[16], float dx, float dy, float dz){
     float t[16] = {
         1, 0, 0, dx,
         0, 1, 0, dy,
         0, 0, 1, dz,
         0, 0, 0, 1 };
-        
-    mat_mul(mat, t, 4, mat);
-    
-}
 
+    mat_mul(mat, t, 4, mat);
+
+}
+//Matriz de rotacion sobre el eje x
 void mat_rotx(float mat[16], float theta){
     mat_rotx2(mat, sin(theta), cos(theta));
 }
-
+//Matriz de rotacion sobre el eje x
 void mat_rotx2(float mat[16], float sintheta, float costheta){
     float rx[16] = {
         1,        0,         0, 0,
@@ -116,11 +137,11 @@ void mat_rotx2(float mat[16], float sintheta, float costheta){
         0,        0,         0, 1 };
     mat_mul(mat, rx, 4, mat);
 }
-
+//Matriz de rotacion sobre el eje y
 void mat_roty(float mat[16], float theta){
     mat_roty2(mat, sin(theta), cos(theta));
 }
-
+//Matriz de rotacion sobre el eje y
 void mat_roty2(float mat[16], float sintheta, float costheta){
     float ry[16] = {
        costheta, 0, sintheta, 0,
@@ -129,11 +150,11 @@ void mat_roty2(float mat[16], float sintheta, float costheta){
               0, 0,        0, 1 };
     mat_mul(mat, ry, 4, mat);
 }
-
+//Matriz de rotacion sobre el eje z
 void mat_rotz(float mat[16], float theta){
     mat_rotz2(mat, sin(theta), cos(theta));
 }
-
+//Matriz de rotacion sobre el eje z
 void mat_rotz2(float mat[16], float sintheta, float costheta){
     float rz[16] = {
       costheta, -sintheta, 0, 0,
@@ -141,9 +162,9 @@ void mat_rotz2(float mat[16], float sintheta, float costheta){
              0,         0, 1, 0,
              0,         0, 0, 1 };
     mat_mul(mat, rz, 4, mat);
-    
-}
 
+}
+//Matriz de escalamiento
 void mat_scale(float mat[16], float sx, float sy, float sz){
     float s[16] = {
         sx,  0,  0, 0,
@@ -151,175 +172,141 @@ void mat_scale(float mat[16], float sx, float sy, float sz){
          0,  0, sz, 0,
          0,  0,  0, 1 };
     mat_mul(mat, s, 4, mat);
-    
-}
 
-void calc_viewport(float *xmin, float *ymin, float *xmax, float *ymax){
+}
+//Funcion que calcula viewport, lo cual nos permite deducir la razon de escalamiento entre la pantalla y el objeto
+void calc_viewport(float *amin, float *bmin, float *amax, float *bmax,int plano){
+    //Definimos indices para determinar en que plano buscaremos los puntos
+    int j,k;
+    switch(plano){
+        case 0:
+            j,k=0,1;
+            break;
+        case 1:
+            j,k=0,2;
+            break;
+        case 2:
+            j,k = 1,2;
+            break;
+        default:
+            printf("No deberia de estar pasando esto");
+    }
+    //Buscamos el minimo y maximo de los puntos en ambos ejes.
 	for (int i = 0; i < N; ++i)
 	{
-		if(triangles[i].p1[0] < *xmin) *xmin = triangles[i].p1[0];
-		if(triangles[i].p1[1] < *ymin) *ymin = triangles[i].p1[1];
-		if(triangles[i].p1[0] > *xmax) *xmax = triangles[i].p1[0];
-		if(triangles[i].p1[1] > *ymax) *ymax = triangles[i].p1[1];
+		if(triangles[i].p1[j] < *xmin) *xmin = triangles[i].p1[j];
+		if(triangles[i].p1[k] < *ymin) *ymin = triangles[i].p1[k];
+		if(triangles[i].p1[j] > *xmax) *xmax = triangles[i].p1[j];
+		if(triangles[i].p1[k] > *ymax) *ymax = triangles[i].p1[k];
 
-		if(triangles[i].p2[0] < *xmin) *xmin = triangles[i].p2[0];
-		if(triangles[i].p2[1] < *ymin) *ymin = triangles[i].p2[1];
-		if(triangles[i].p2[0] > *xmax) *xmax = triangles[i].p2[0];
-		if(triangles[i].p2[1] > *ymax) *ymax = triangles[i].p2[1];
+		if(triangles[i].p2[j] < *xmin) *xmin = triangles[i].p2[j];
+		if(triangles[i].p2[k] < *ymin) *ymin = triangles[i].p2[k];
+		if(triangles[i].p2[j] > *xmax) *xmax = triangles[i].p2[j];
+		if(triangles[i].p2[k] > *ymax) *ymax = triangles[i].p2[k];
 
-		if(triangles[i].p3[0] < *xmin) *xmin = triangles[i].p3[0];
-		if(triangles[i].p3[1] < *ymin) *ymin = triangles[i].p3[1];
-		if(triangles[i].p3[0] > *xmax) *xmax = triangles[i].p3[0];
-		if(triangles[i].p3[1] > *ymax) *ymax = triangles[i].p3[1];
+		if(triangles[i].p3[a] < *xmin) *xmin = triangles[i].p3[j];
+		if(triangles[i].p3[b] < *ymin) *ymin = triangles[i].p3[k];
+		if(triangles[i].p3[a] > *xmax) *xmax = triangles[i].p3[j];
+		if(triangles[i].p3[b] > *ymax) *ymax = triangles[i].p3[k];
 	}
 }
-
-void calc_mat(float m[16]){
+//Funcion que calcula la matriz compuesta que usaremos para proyectar en pantalla.
+void calc_mat(float m[16],int plano){
+    //definimos la matriz M como la identidad
 	mat_id(m);
-	m[10] = 0; //proyectar en xy
-	
-	float xmin, ymin, xmax, ymax;
-	calc_viewport(&xmin, &ymin, &xmax, &ymax);
-	float w = (xmax-xmin);
-	float h = (ymax-ymin);
-	float sx = width/w, sy = height/h, dx = 0, dy = 0;
-	if(sx < sy) sy = sx;
-	else sx = sy;
-	
-	/*if(h > w){
-		sx = sy*w/h;
-		dx = width*(1-h/w)/2;
-	} else {
-		sy = sx*h/w;
-		dy = height*(1-w/h)/2;
-	}
-	*/
-	
-	float dx = (width-(xmax*sx))/2, dy = ((ymax*sy)-height)/2;
-	mat_trans(m, dx, dy, 0);
-	mat_scale(m, sx, sy, 1);
-	mat_trans(m, -xmin, -ymin, 0);
+	//Definimos el plano a proyectar. Variable plano = [0,2] donde 0 = xy, 1 = xz, 2 = yz
+	m[10-plano*5] = 0;
+    //Definimos las variables maximas y minimas que cubre nuestro objeto.
+	float amin, bmin, amax, bmax;
+	//Calculamos sus valores
+	calc_viewport(&amin, &bmin, &amax, &bmax,plano);
+	//Calculamos el ancho y alto del objeto
+	float w = (amax-amin);
+	float h = (bmax-bmin);
+	//Calculamos el factor de escalamiento para la transformacion windows a viewport
+	float sa = width/w, sb = height/h;
+	//Se comparan las escalas en 'a' y 'b' y se elije la menor, para que el objeto conserve su escala original
+	if(sa < sb) sb = sa;
+	else sa = sb;
+    //Calculamos las distancias necesarias para que el objeto quede centrado
+	float da = (width-(w*sa))/2-1, db = -((h*sb)-height)/2-1;
+	//Calculamos la matriz de transformacion compuesta
+	mat_trans(m, da, db, 0);
+	mat_scale(m, sa, sb, 1);
+	mat_trans(m, -amin, -bmin, 0);
+	//DEBUG: imprimimos los puntos
 	for(int i=0; i<4; ++i)
 		printf("[%f,%f,%f,%f]\n", m[4*i], m[4*i+1], m[4*i+2], m[4*i+3]);
-	
-}
 
-void proyectar(float m[16], float a[], struct Trianglew t){
+}
+//Funcion que proyecta y dibuja los triangulos en el plano deseado
+void proyectar(float m[16], float a[], struct Trianglew t, int plano){
+    ///TODO:Proyeccion axonometrica
+    //arreglos auxiliares para dibujar puntos
 	float r1[4], r2[4], r3[4];
+    //multiplicamos los puntos de el triangulo por la matriz compuesta
 	mat_mul(m, t.p1, 1, r1);
 	mat_mul(m, t.p2, 1, r2);
 	mat_mul(m, t.p3, 1, r3);
-	draw_pixel(a, r1[0], r1[1], 1,1,1);
-	draw_pixel(a, r2[0], r2[1], 1,1,1);
-	draw_pixel(a, r3[0], r3[1], 1,1,1);
-	//dibujar_linea(a, (int)r1[0], (int)r1[1], (int)r2[0], (int)r2[1]);
-	//dibujar_linea(a, (int)r1[0], (int)r1[1], (int)r3[0], (int)r3[1]);
-	//dibujar_linea(a, (int)r3[0], (int)r3[1], (int)r2[0], (int)r2[1]);
-}
 
-void dibujar_linea(float a[], int _x1, int _y1, int _x2, int _y2){
-	if( _x1 < 0 || _x1 > width || _x2 < 0 || _x2 > width ||
-		_y1 < 0 || _y1 > height|| _y2 < 0 || _y2 > height )
-		printf("(%d,%d), (%d,%d)\n", _x1, _y1, _x2, _y2);
-	if(_x2-_x1 == 0){
-		int ymin = _y1<_y2 ? _y1 : _y2;
-		int ymax = _y1<_y2 ? _y2 : _y1;
-		for(int i=ymin; i<=ymax; ++i){
-			draw_pixel(a, _x1, i, 1, 1, 1);
-		}
-		//glDrawPixels(width, height, GL_RGB, GL_FLOAT, a);
-		//glutSwapBuffers();
-		return;
+	//Dibujamos los puntos, usando la variable plano para decidir sobre cual plano proyectar
+
+
+	///TODO: Verificar, No se si esto este correcto, solo asumi que teniamos que cambiar sobre plano dibujamos,
+	///aun se dibujan los mismos puntos que en el caso del plano xy
+	switch (plano){
+        case 0: // Plano xy
+            glVertex3f(r1[0], r1[1], 0);
+            glVertex3f(r2[0], r2[1], 0);
+            glVertex3f(r1[0], r1[1], 0);
+            glVertex3f(r3[0], r3[1], 0);
+            glVertex3f(r3[0], r3[1], 0);
+            glVertex3f(r2[0], r2[1], 0);
+            break;
+        case 1: //Plano xz
+            glVertex3f(r1[0], 0, r1[2]);
+            glVertex3f(r2[0], 0, r2[2]);
+            glVertex3f(r1[0], 0, r1[2]);
+            glVertex3f(r3[0], 0, r3[2]);
+            glVertex3f(r3[0], 0, r3[2]);
+            glVertex3f(r2[0], 0, r2[2]);
+            break;
+        case 2: //Plano yz
+            glVertex3f(0, r1[1], r1[2]);
+            glVertex3f(0, r2[1], r2[2]);
+            glVertex3f(0, r1[1], r1[2]);
+            glVertex3f(0, r3[1], r3[2]);
+            glVertex3f(0, r3[1], r3[2]);
+            glVertex3f(0, r2[1], r2[2]);
+            break;
 	}
-	float m = (float)(_y2-_y1)/(_x2-_x1);
-	if(m < -1)
-		pendiente_menor_menos1(a, _x1, _y1, _x2, _y2);
-	else if (m < 0)
-		pendiente_entre_menos1y0(a, _x1, _y1, _x2, _y2);
-	else if (m < 1)
-		pendiente_entre_1y0(a, _x1, _y1, _x2, _y2);
-	else pendiente_mayor_1(a, _x1, _y1, _x2, _y2);
-}
 
-void pendiente_menor_menos1(float *a, int x1, int y1, int x2, int y2){
-	int dx = x2-x1, dy = y2-y1;
-	int bdx = y1*dx - x1*dy;
-	int x = x1<x2 ? x1 : x2;
-	int y = x1<x2 ? y1 : y2;
-	draw_pixel(a, x, y, 1, 1, 1);
-	int ymax = x1<x2 ? y2 : y1;
-	while(y>ymax){
-		if(dy*(x+0.5f)-dx*(y-1)+bdx > 0)
-			draw_pixel(a, ++x, --y, 1, 1, 1);
-		else draw_pixel(a, x, --y, 1, 1, 1);
-		
+
 	}
 }
-
-void pendiente_entre_menos1y0(float *a, int x1, int y1, int x2, int y2){
-	int dx = x2-x1, dy = y2-y1;
-	int bdx = y1*dx - x1*dy;
-	int x = x1<x2 ? x1 : x2;
-	int y = x1<x2 ? y1 : y2;
-	draw_pixel(a, x, y, 1, 1, 1);
-	int xmax = x1<x2 ? x2 : x1;
-	while(x<xmax){
-		if(dy*(x+1)-dx*(y-0.5f)+bdx < 0)
-			draw_pixel(a, ++x, --y, 1, 1, 1);
-		else draw_pixel(a, ++x, y, 1, 1, 1);
-	}
-}
-
-void pendiente_entre_1y0(float *a, int x1, int y1, int x2, int y2){
-	int dx = x2-x1, dy = y2-y1;
-	int bdx = y1*dx - x1*dy;
-	int x = x1<x2 ? x1 : x2;
-	int y = x1<x2 ? y1 : y2;
-	draw_pixel(a, x, y, 1, 1, 1);
-	int xmax = x1<x2 ? x2 : x1;
-	while(x<xmax){
-		if(dy*(x+1)-dx*(y+0.5f)+bdx < 0)
-			draw_pixel(a, ++x, y, 1, 1, 1);
-		else draw_pixel(a, ++x, ++y, 1, 1, 1);
-		
-	}
-}
-
-void pendiente_mayor_1(float *a, int x1, int y1, int x2, int y2){
-	int dx = x2-x1, dy = y2-y1;
-	int bdx = y1*dx - x1*dy;
-	int x = x1<x2 ? x1 : x2;
-	int y = x1<x2 ? y1 : y2;
-	draw_pixel(a, x, y, 1, 1, 1);
-	int ymax = x1<x2 ? y2 : y1;
-	//int *i;
-	//printf("%d", sizeof(float)*height*width*3);
-	//scanf("%d", i);
-	while(y<ymax){
-		if(dy*(x+0.5f)-dx*(y+1)+bdx < 0)
-			draw_pixel(a, ++x, ++y, 1, 1, 1);
-		else draw_pixel(a, x, ++y, 1, 1, 1);
-	}
-}
-
+//Preparar la interfaz de openGL
 void setup(){
-	glClearColor(1.0f, 1.0f, 1.0f, 1.0f);
+	glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
 }
 
 void draw_pixel(float *a, int x, int y, float r, float g, float b){
-	printf("(%d, %d)\n", x, y);
+	//printf("(%d, %d)\n", x, y);
 	int p = y*width*3+x*3;
 	a[p] = r;
 	a[p+1] = g;
 	a[p+2] = b;
 }
-
+//Funcion para procesar los datos almacenados en los archivos
 void abrir_archivo_y_procesa(struct Trianglew tris[]){
+    //Declaramos las variables necesarias para procesar archivos
 	FILE *file;
 	float buff;
+	//Abrimos el archivo con los puntos
 	file = fopen("OBJETOS-3D/itokawa_f0049152.tri", "r");
 	int i=0;
+	//Leemos todo el archivo
 	while(fscanf(file, "%f", &buff) != EOF){
+        //Obtenemos las tres coordenadas del primer punto del triangulo
 		tris[i].p1[0] = buff;
 		fscanf(file, "%f", &buff);
 		tris[i].p1[1] = buff;
@@ -327,6 +314,7 @@ void abrir_archivo_y_procesa(struct Trianglew tris[]){
 		tris[i].p1[2] = buff;
 		tris[i].p1[3] = 1;
 		fscanf(file, "%f", &buff);
+        //Hacemos lo mismo para el segundo punto
 		tris[i].p2[0] = buff;
 		fscanf(file, "%f", &buff);
 		tris[i].p2[1] = buff;
@@ -334,6 +322,7 @@ void abrir_archivo_y_procesa(struct Trianglew tris[]){
 		tris[i].p2[2] = buff;
 		tris[i].p2[3] = 1;
 		fscanf(file, "%f", &buff);
+		//Y lo mismo para el tercer punto
 		tris[i].p3[0] = buff;
 		fscanf(file, "%f", &buff);
 		tris[i].p3[1] = buff;
@@ -343,5 +332,6 @@ void abrir_archivo_y_procesa(struct Trianglew tris[]){
 		fscanf(file, "%f", &buff);
 		++i;
 	}
+	//Cerramos el archivo
 	fclose(file);
 }
